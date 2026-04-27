48e67a8c8573895d79339afcbfe562ad80e57dda
rkriyakumari077-cmyk <rkriyakumari077@gmail.com>
2026-04-27 12:42:12 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,10 @@
+def greet(input):
+    if "hello" in input:
+        return "Hello, there!"
+    else:
+        return "I'm not sure what you mean "
+
+
+greeting = greet("hello, computer")
+print(greeting+ "Riya")
+
